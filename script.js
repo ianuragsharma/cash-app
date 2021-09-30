20154bb1725ffc8f3d381b0938479929ac836cfa
Anurag Sharma <anuragsharma0711@gmail.com>
2021-09-30 11:07:59 +0530

    fix: input was taking negative values

--- script.js
+++ script.js
@@ -6,13 +6,25 @@ const bill = document.getElementById("bill-amount");
 const noReturn = document.getElementById("no-return");
 const table = document.getElementById("table");
 const cash = document.getElementById("cash-given");
+const amountReturned = document.getElementById("amount-returned");
 
 errorMsgAmountNegative.style.display = "none";
 errorMsgLessCash.style.display = "none";
+amountReturned.style.display = "none";
 
+let notes = [2000, 500, 100, 20, 10, 5, 1];
+function clearNotes(notes) {
+  for (let i = 0; i < notes.length; i++) {
+    document.getElementById(notes[i]).innerText = "";
+  }
+}
 nextBtn.addEventListener("click", function () {
+  clearNotes(notes);
   if (bill.value <= 0) {
     errorMsgAmountNegative.style.display = "block";
+    table.style.display = "none";
+    amountReturned.style.display = "none";
+    document.getElementById("cash").style.display = "none";
   } else {
     errorMsgAmountNegative.style.display = "none";
     document.getElementById("cash").style.display = "block";
@@ -20,23 +32,35 @@ nextBtn.addEventListener("click", function () {
 });
 
 checkBtn.addEventListener("click", function () {
+  clearNotes(notes);
+
   let billAmount = Number(bill.value);
   let cashGiven = Number(cash.value);
 
   if (cashGiven < billAmount) {
     errorMsgLessCash.style.display = "block";
     table.style.display = "none";
+    amountReturned.style.display = "none";
   } else if (cashGiven === billAmount) {
     errorMsgLessCash.style.display = "none";
     noReturn.style.display = "block";
+    amountReturned.style.display = "none";
+
     table.style.display = "none";
   } else {
     errorMsgLessCash.style.display = "none";
     noReturn.style.display = "none";
+    let amount = cashGiven - billAmount;
+    console.log(amount);
+    amountReturned.style.display = "block";
+    amountReturned.innerText = "The amount returned is " + amount;
     table.style.display = "block";
   }
-
-  let notes = [2000, 500, 100, 20, 10, 5, 1];
+  function clearNotes(notes) {
+    for (let i = 0; i < notes.length; i++) {
+      document.getElementById(notes[i]).innerText = "";
+    }
+  }
   let balance = cashGiven - billAmount;
   for (let i = 0; i < notes.length; i++) {
     if (balance > 0 && balance >= notes[i]) {
